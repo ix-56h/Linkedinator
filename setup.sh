b7a96e03df7b4a9ba8531376c1419d3f359f470f
Phalor <0x00fi@protonmail.com>
2020-03-11 21:38:59 +0100

    creation of drivers directory with setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 python3 -m pip install -r requirements.txt --user
-#wget https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_mac64.zip && mv chromedriver_mac64.zip drivers/. && unzip drivers/chromedriver_mac64.zip && rm drivers/chromedriver_mac64.zip
+mkdir drivers
